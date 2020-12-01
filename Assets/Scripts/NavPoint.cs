4d7dd4c1ad0705ee8fa460f7829292ea41f678ca
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2020-12-01 19:26:40 +0100

    InitialCommit

---
+++ Assets/Scripts/NavPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavPoint : MonoBehaviour
+{
+    [SerializeField] private NavPoint[] _neighbourNavPoints;
+
+    public NavPoint[] NeighbourNavPoints{
+        get { return _neighbourNavPoints; }
+    }
+}
---
+++ Assets/Scripts/UnitPatrolBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitPatrolBehaviour : MonoBehaviour
+{
+    [SerializeField] private NavPoint[] _patrolPoints;
+    [SerializeField] private NavPoint _currentDestination;
+    [SerializeField] private NavMeshAgent _agent;
+
+    private void GetNextPatrolPoint(){
+        NavPoint[] possibleToTravel = _currentDestination.NeighbourNavPoints;
+        _currentDestination = possibleToTravel[ Random.Range(0, possibleToTravel.Length)];
+        if( _currentDestination == null ){
+            return;
+        }
+       _agent.destination = _currentDestination.transform.position;
+    }
+
+    void Update()
+    {
+        if (!_agent.pathPending && _agent.remainingDistance < 0.5f){
+            GetNextPatrolPoint();
+        }
+    }
+}
